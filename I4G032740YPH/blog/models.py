32016db0c9ac1ef58dbb1796d0bc71a787ce4921
Adetunji taiwo <adetunjitaiwo543@gmail.com>
2022-06-19 11:07:22 -1200

    django model

---
+++ I4G032740YPH/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+# Create your models here.
+
+
+class post(models.Model):
+    Name = models.CharField(max_length=200)
+    Text = models.TextField(max_length=300)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
